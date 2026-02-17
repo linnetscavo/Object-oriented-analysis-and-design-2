00862c067963579cb029b73d3d366998607b70d7
Дарья <fievdogs@gmail.com>
2026-02-17 12:04:33 +0700

    Create my.cs

---
+++ lab01/my.cs
@@ -0,0 +1,17 @@
+class Director
+
+class ComputerCreator
+
+    
+
+class Computer
+{
+    public string CPU { get; set; }           
+    public string GPU { get; set; }           
+    public string RAM { get; set; }           
+    public string Storage { get; set; } 
+    public string Motherboard { get; set; }
+    public string PSU { get; set; }
+    public string Case { get; set; }
+    public string Cooling { get; set; }     
+}
